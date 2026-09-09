116c7dd26c79b9405d40debd45d3372f14e97fb4
Max <max@polyth.top>
2026-09-09 13:23:56 +0300

    feat(mobile): register Android discovery plugin

--- apps/mobile/android/app/src/main/java/com/polyth/mobile/MainActivity.java
+++ apps/mobile/android/app/src/main/java/com/polyth/mobile/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(PolythLinkPlugin.class);
+        registerPlugin(PolythDiscoveryPlugin.class);
         super.onCreate(savedInstanceState);
     }
 }
