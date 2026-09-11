5bb864de0af7dcdbccdbb640d8075dff83cb3327
Max <max@polyth.top>
2026-09-11 15:17:57 +0300

    feat(pi): export native RPC adapter

--- packages/backend-pi/src/index.ts
+++ packages/backend-pi/src/index.ts
@@ -1,1 +1,3 @@
 export { default } from "./serverEntry.ts";
+export { createPiRpc, type PiRpc, type PiRpcEvent, type PiRpcModel, type PiRpcSessionStats, type PiRpcState } from "./rpc.ts";
+export { createPiRuntime, PI_CAPABILITIES } from "./runtime.ts";
