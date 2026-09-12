8fdbb80f81304e00e47cb039acb2ad9ed6171101
Max <max@polyth.top>
2026-09-12 07:38:37 +0300

    Apply mobile navigator safe-area polish

--- apps/web/src/components/Navigation.tsx
+++ apps/web/src/components/Navigation.tsx
@@ -1,5 +1,6 @@
 import Sidebar from "./Sidebar.tsx";
 import MobileNavigator from "./mobile/MobileNavigator.tsx";
+import "./mobile/MobileNavigatorPolish.css";
 import { useShellMode } from "../responsiveShell.ts";
 
 export default function Navigation() {
