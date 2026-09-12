7c6c64e57c37ef5550ca6e027ea8c6ff501c9a6a
Max <max@polyth.top>
2026-09-12 08:18:25 +0300

    style(mobile): load premium workspace panel skin

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -7,6 +7,7 @@ import { prepareMobileLaunch } from "@polyth/mobile/runtime";
 import "./styles.css";
 import "./moduleContent.css";
 import "./composerAdaptive.css";
+import "./workspacePanelPremium.css";
 import "./motion.css";
 import "./chatMotion.ts";
 
